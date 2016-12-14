a151abde9015a8372df4684eacfddf643e909097
sh7ning <sh7ning@qq.com>
2016-12-14 17:30:11 +0800

    默认url没有后缀

--- app/config/main.php
+++ app/config/main.php
@@ -9,9 +9,5 @@ return array(
     'app_path'          => dirname(__DIR__),
     'storage_path'      => dirname(__DIR__) . '/storage',
     'static'            => 'http://s.sh7nestatic.com', //静态资源
-    'dispatch'          => array( //路由相关配置
-        'url_suffix'            => 'jsp', //此处数组为配置文件，URL伪静态后缀设置 URL_HTML_SUFFIX:html/html/asp/jsp/cgi
-        'path_separator'        => '/', //可以为 /|-|~
-    ),
     'xhprof_dir'        =>  __DIR__ . '/../../public/xhprof',
 );

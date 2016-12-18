700ae87e1a89ba498306e9883d2a4a0bdfa823f7
sh7ning <sh7ning@qq.com>
2016-12-19 01:13:00 +0800

    修复rpc中ctx修改后的实例化方式

--- public/rpc.php
+++ public/rpc.php
@@ -5,5 +5,5 @@ ini_set('display_errors', 1);
 ini_set('error_reporting', E_ALL);
 require __DIR__ . '/../ctx_base/vendor/autoload.php';
 
-$ctx = Tree6bee\Ctx\Ctx::getInstance(__DIR__ . '/../ctx_base');
+$ctx = Ctx\Ctx::getInstance();
 (new Tree6bee\Ctx\Rpc\Server)->run($ctx);

d1996d92bde8ac4a3386937bfbfb7018959cc0cc
sh7ning <sh7ning@qq.com>
2016-12-19 00:30:55 +0800

    更新类路径

--- public/index.php
+++ public/index.php
@@ -47,7 +47,7 @@
 require __DIR__ . '/../ctx_base/vendor/autoload.php';
 
 //如果采用composer统一处理 这里可以去掉
-new \Tree6bee\Ctx\Loader\Psr4(array(
+new \Tree6bee\Support\Ctx\Loader\Psr4(array(
     'App\\' => __DIR__ . '/../app',
 ));
 
